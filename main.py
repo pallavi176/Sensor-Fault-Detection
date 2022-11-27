6899feb114c9c9daa9475619da471428a9162df1
Pallavi Saxena <saxena.pallavi7211@gmail.com>
2022-11-27 17:12:09 +0530

    data ingestion pipeline executed

--- main.py
+++ main.py
@@ -1,5 +1,13 @@
 from sensor.configuration.mongo_db_connection import MongoDBClient
+from sensor.exception import SensorException
+import os,sys
+from sensor.logger import logging
+from sensor.pipeline import training_pipeline
+from sensor.pipeline.training_pipeline import TrainPipeline
+import os
 
 if __name__=="__main__":
-    mongodb_client = MongoDBClient()
-    print("Collection names: ", mongodb_client.database.list_collection_names())
+    # mongodb_client = MongoDBClient()
+    # print("Collection names: ", mongodb_client.database.list_collection_names())
+    training_pipeline = TrainPipeline()
+    training_pipeline.run_pipeline()
