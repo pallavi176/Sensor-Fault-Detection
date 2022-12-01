54ea030e6dc514166754ba10dc2c122d66e5445b
Pallavi Saxena <saxena.pallavi7211@gmail.com>
2022-12-01 21:59:45 +0530

    data tranformation step done

--- sensor/entity/config_entity.py
+++ sensor/entity/config_entity.py
@@ -1,10 +1,10 @@
 import os
 from datetime import datetime
-from sensor.constants  import training_pipeline
+from sensor.constants import training_pipeline
 
 class TrainingPipelineConfig:
 
-    def __init__(self,timestamp=datetime.now()):
+    def __init__(self, timestamp = datetime.now()):
         timestamp = timestamp.strftime("%m_%d_%Y_%H_%M_%S")
         self.pipeline_name: str = training_pipeline.PIPELINE_NAME
         self.artifact_dir: str = os.path.join(training_pipeline.ARTIFACT_DIR, timestamp)
@@ -13,7 +13,7 @@ class TrainingPipelineConfig:
 
 class DataIngestionConfig:
     
-        def __init__(self,training_pipeline_config:TrainingPipelineConfig):
+        def __init__(self, training_pipeline_config: TrainingPipelineConfig):
             self.data_ingestion_dir: str = os.path.join(
                 training_pipeline_config.artifact_dir, training_pipeline.DATA_INGESTION_DIR_NAME
             )
@@ -32,7 +32,8 @@ class DataIngestionConfig:
 
 class DataValidationConfig:
 
-    def __init__(self,training_pipeline_config:TrainingPipelineConfig):
+    def __init__(self, training_pipeline_config: TrainingPipelineConfig):
+
         self.data_validation_dir: str = os.path.join( training_pipeline_config.artifact_dir, training_pipeline.DATA_VALIDATION_DIR_NAME)
         self.valid_data_dir: str = os.path.join(self.data_validation_dir, training_pipeline.DATA_VALIDATION_VALID_DIR)
         self.invalid_data_dir: str = os.path.join(self.data_validation_dir, training_pipeline.DATA_VALIDATION_INVALID_DIR)
@@ -45,4 +46,18 @@ class DataValidationConfig:
             training_pipeline.DATA_VALIDATION_DRIFT_REPORT_DIR,
             training_pipeline.DATA_VALIDATION_DRIFT_REPORT_FILE_NAME,
         )
+
+
+class DataTransformationConfig:
+
+    def __init__(self, training_pipeline_config: TrainingPipelineConfig):
+
+        self.data_transformation_dir: str = os.path.join(training_pipeline_config.artifact_dir, training_pipeline.DATA_TRANSFORMATION_DIR_NAME)
+        self.transformed_train_file_path: str = os.path.join(self.data_transformation_dir, training_pipeline.DATA_TRANSFORMATION_TRANSFORMED_DATA_DIR,
+            training_pipeline.TRAIN_FILE_NAME.replace("csv", "npy"))
+        self.transformed_test_file_path: str = os.path.join(self.data_transformation_dir, training_pipeline.DATA_TRANSFORMATION_TRANSFORMED_DATA_DIR,
+            training_pipeline.TEST_FILE_NAME.replace("csv", "npy"))
+        self.transformed_object_file_path: str = os.path.join(self.data_transformation_dir, training_pipeline.DATA_TRANSFORMATION_TRANSFORMED_OBJECT_DIR,
+            training_pipeline.PREPROCSSING_OBJECT_FILE_NAME)
+
 
--- sensor/pipeline/training_pipeline.py
+++ sensor/pipeline/training_pipeline.py
@@ -1,9 +1,10 @@
 import os
 import sys
-from sensor.entity.config_entity import TrainingPipelineConfig, DataIngestionConfig, DataValidationConfig
-from sensor.entity.artifact_entity import DataIngestionArtifact, DataValidationArtifact
+from sensor.entity.config_entity import TrainingPipelineConfig, DataIngestionConfig, DataValidationConfig, DataTransformationConfig
+from sensor.entity.artifact_entity import DataIngestionArtifact, DataValidationArtifact, DataTransformationArtifact
 from sensor.components.data_ingestion import DataIngestion
 from sensor.components.data_validation import DataValidation
+from sensor.components.data_transformation import DataTransformation
 from sensor.constants.s3_bucket import TRAINING_BUCKET_NAME
 from sensor.constants.training_pipeline import SAVED_MODEL_DIR
 from sensor.exception import SensorException
@@ -30,11 +31,24 @@ class TrainPipeline:
     def start_data_validaton(self, data_ingestion_artifact: DataIngestionArtifact) -> DataValidationArtifact:
         try:
             data_validation_config = DataValidationConfig(training_pipeline_config=self.training_pipeline_config)
-            data_validation = DataValidation(data_ingestion_artifact=data_ingestion_artifact,
-            data_validation_config = data_validation_config
+            data_validation = DataValidation(
+                data_ingestion_artifact=data_ingestion_artifact,
+                data_validation_config = data_validation_config
             )
             data_validation_artifact = data_validation.initiate_data_validation()
             return data_validation_artifact
+        except  Exception as e:
+            raise  SensorException(e, sys)
+
+    def start_data_transformation(self, data_validation_artifact: DataValidationArtifact):
+        try:
+            data_transformation_config = DataTransformationConfig(training_pipeline_config=self.training_pipeline_config)
+            data_transformation = DataTransformation(
+                data_validation_artifact=data_validation_artifact,
+                data_transformation_config=data_transformation_config
+            )
+            data_transformation_artifact = data_transformation.initiate_data_transformation()
+            return data_transformation_artifact
         except  Exception as e:
             raise  SensorException(e, sys)
 
@@ -43,6 +57,7 @@ class TrainPipeline:
             TrainPipeline.is_pipeline_running=True
             data_ingestion_artifact: DataIngestionArtifact = self.start_data_ingestion()
             data_validation_artifact = self.start_data_validaton(data_ingestion_artifact=data_ingestion_artifact)
+            data_transformation_artifact = self.start_data_transformation(data_validation_artifact=data_validation_artifact)
         except  Exception as e:
             TrainPipeline.is_pipeline_running = False
             raise  SensorException(e, sys)
