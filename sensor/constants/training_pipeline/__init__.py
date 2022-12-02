f582eaa4b3e526e70d3058f1ecc78030d0c2f3c4
Pallavi Saxena <saxena.pallavi7211@gmail.com>
2022-12-02 21:56:13 +0530

    model trainer baseline added

--- sensor/constants/training_pipeline/__init__.py
+++ sensor/constants/training_pipeline/__init__.py
@@ -1,9 +1,9 @@
 import os
 from sensor.constants.s3_bucket import TRAINING_BUCKET_NAME
 
-SAVED_MODEL_DIR =os.path.join("saved_models")
+SAVED_MODEL_DIR = os.path.join("saved_models")
 
-# defining common constant variable for training pipeline
+# Defining common constant variable for training pipeline
 TARGET_COLUMN = "class"
 PIPELINE_NAME: str = "sensor"
 ARTIFACT_DIR: str = "artifact"
@@ -18,9 +18,7 @@ SCHEMA_FILE_PATH = os.path.join("config", "schema.yaml")
 SCHEMA_DROP_COLS = "drop_columns"
 
 
-"""
-Data Ingestion related constant start with DATA_INGESTION VAR NAME
-"""
+# Data Ingestion related constant start with DATA_INGESTION VAR NAME
 DATA_INGESTION_COLLECTION_NAME: str = "car"
 DATA_INGESTION_DIR_NAME: str = "data_ingestion"
 DATA_INGESTION_FEATURE_STORE_DIR: str = "feature_store"
@@ -28,10 +26,7 @@ DATA_INGESTION_INGESTED_DIR: str = "ingested"
 DATA_INGESTION_TRAIN_TEST_SPLIT_RATION: float = 0.2
 
 
-"""
-Data Validation realted contant start with DATA_VALIDATION VAR NAME
-"""
-
+# Data Validation realted contant start with DATA_VALIDATION VAR NAME
 DATA_VALIDATION_DIR_NAME: str = "data_validation"
 DATA_VALIDATION_VALID_DIR: str = "validated"
 DATA_VALIDATION_INVALID_DIR: str = "invalid"
@@ -39,11 +34,16 @@ DATA_VALIDATION_DRIFT_REPORT_DIR: str = "drift_report"
 DATA_VALIDATION_DRIFT_REPORT_FILE_NAME: str = "report.yaml"
 
 
-"""
-Data Transformation ralated constant start with DATA_TRANSFORMATION VAR NAME
-"""
-
+# Data Transformation ralated constant start with DATA_TRANSFORMATION VAR NAME
 DATA_TRANSFORMATION_DIR_NAME: str = "data_transformation"
 DATA_TRANSFORMATION_TRANSFORMED_DATA_DIR: str = "transformed"
 DATA_TRANSFORMATION_TRANSFORMED_OBJECT_DIR: str = "transformed_object"
+
+
+# Model Trainer ralated constant start with MODE TRAINER VAR NAME
+MODEL_TRAINER_DIR_NAME: str = "model_trainer"
+MODEL_TRAINER_TRAINED_MODEL_DIR: str = "trained_model"
+MODEL_TRAINER_TRAINED_MODEL_NAME: str = "model.pkl"
+MODEL_TRAINER_EXPECTED_SCORE: float = 0.6
+MODEL_TRAINER_OVER_FIITING_UNDER_FITTING_THRESHOLD: float = 0.05
 
