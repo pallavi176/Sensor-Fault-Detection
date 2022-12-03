fa9c2167d2f01331700d2b4224c9e684f589037d
Pallavi Saxena <saxena.pallavi7211@gmail.com>
2022-12-03 16:45:52 +0530

    fast api created

---
+++ main.py
@@ -0,0 +1,88 @@
+import os
+import sys
+from fastapi import FastAPI
+from fastapi.responses import Response
+from fastapi.middleware.cors import CORSMiddleware
+from uvicorn import run as app_run
+from starlette.responses import RedirectResponse
+from sensor.configuration.mongo_db_connection import MongoDBClient
+from sensor.pipeline.training_pipeline import TrainPipeline
+from sensor.ml.model.estimator import ModelResolver, TargetValueMapping
+from sensor.constants.training_pipeline import SAVED_MODEL_DIR
+from sensor.constants.application import APP_HOST, APP_PORT
+from sensor.constants.env_variable import MONGODB_URL_KEY
+from sensor.utils.main_utils import read_yaml_file, load_object
+from sensor.exception import SensorException
+from sensor.logger import logging
+
+
+env_file_path=os.path.join(os.getcwd(), "env.yaml")
+
+def set_env_variable(env_file_path):
+
+    if os.getenv('MONGO_DB_URL',None) is None:
+        env_config = read_yaml_file(env_file_path)
+        os.environ['MONGO_DB_URL'] = env_config['MONGO_DB_URL']
+
+app = FastAPI()
+origins = ["*"]
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+@app.get("/", tags=["authentication"])
+async def index():
+    return RedirectResponse(url="/docs")
+
+@app.get("/train")
+async def train_route():
+    try:
+        train_pipeline = TrainPipeline()
+        if train_pipeline.is_pipeline_running:
+            return Response("Training pipeline is already running.")
+        train_pipeline.run_pipeline()
+        return Response("Training successful !!")
+    except Exception as e:
+        return Response(f"Error Occurred! {e}")
+
+@app.get("/predict")
+async def predict_route():
+    try:
+        #get data from user csv file
+        #conver csv file to dataframe
+
+        df = None
+        model_resolver = ModelResolver(model_dir=SAVED_MODEL_DIR)
+        if not model_resolver.is_model_exists():
+            return Response("Model is not available")
+        
+        best_model_path = model_resolver.get_best_model_path()
+        model = load_object(file_path=best_model_path)
+        y_pred = model.predict(df)
+        df['predicted_column'] = y_pred
+        df['predicted_column'].replace(TargetValueMapping().reverse_mapping(), inplace=True)
+        
+        # decide how to return file to user.
+        
+    except Exception as e:
+        raise Response(f"Error Occured! {e}")
+
+def main():
+    try:
+        # set_env_variable(env_file_path)
+        mongo_db_url = os.getenv(MONGODB_URL_KEY)
+        print(mongo_db_url)
+        training_pipeline = TrainPipeline()
+        training_pipeline.run_pipeline()
+    except Exception as e:
+        print(e)
+        logging.exception(e)
+
+if __name__=="__main__":
+    #main()
+    # set_env_variable(env_file_path)
+    app_run(app, host=APP_HOST, port=APP_PORT)
